1c1267c39e0ac89590f57d52209829c68f5c9524
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-07-28 00:08:53 +0200

    Bug fix and parameter tweak

--- MV_Optimize.m
+++ MV_Optimize.m
@@ -5,12 +5,12 @@ function [ w ] = MV_Optimize( mu, sigma, k )
   Aeq = ones(1,nStocks); beq = 1;
   lb = 0*ones(nStocks,1); ub = ones(nStocks,1);
   options = optimset('Display', 'off') ;
-  [w,~,exitflag] = fmincon(@(w)-objective(w, mu, sigma, k), w0, [], [], Aeq, beq, lb, ub, [], options);
+  [w,~,exitflag] = fmincon(@(w)-expectedUtility(w, mu, sigma, k), w0, [], [], Aeq, beq, lb, ub, [], options);
   if exitflag < 1
     disp('we have a problem')
   end
   
-  function [value] = objective(w, mu, sigma, k)
+  function [value] = expectedUtility(w, mu, sigma, k)
     value = mu*w - k/2*(w'*sigma*w);
   end
 
--- Main.m
+++ Main.m
@@ -13,35 +13,44 @@ return_data = ( stocks_matrix_data(2:end,:) - stocks_matrix_data(1:end-1,:) ) ./
 return_data(abs(return_data) > 0.3) = 0; % Remove stock splits
 
 
-start = 250;
+start = 30;
 step = 10;
 times_to_evaluate = start:step:size(return_data,1);
-store_weights = [];
+T = length(times_to_evaluate);
+risk = [0.5 1 2 3];
 
-figure(2),clf
-hold on
-for k = [0.5 1 2 3]
+store_weights = zeros(T, length(stocks), length(risk));
+store_capital = zeros(T,length(risk));
+
+for ik = 1:length(risk)
+  k = risk(ik);
+  weigths = [];
+  fprintf('Processing k = %.1f \n',k)
   for t = times_to_evaluate
     if t>start
-      new_capital = sum( w .* prod(return_data(t-step:t,:) + 1 ,1)' * capital(end) );
+      new_capital = sum( w .* prod(return_data(t-step+1:t,:) + 1 ,1)' * capital(end) );
       capital = [capital; new_capital];
     else
       capital = 1;
     end
 
-    mu = mean(return_data(1:t,:),1);
-    sigma = cov(return_data(1:t,:));
+    mu = mean(return_data(t-start+1:t,:),1);
+    sigma = cov(return_data(t-start+1:t,:));
 
     w = MV_Optimize(mu, sigma, k);
 
-    store_weights = [store_weights; w'];
+    weigths = [weigths; w'];
   end
-  plot(capital)
+  store_capital(:,ik) = capital;
+  store_weights(:,:,ik) = weigths;
 end
 
+figure(2),clf
+hold on
+plot(store_capital)
 index_data_to_show = index_matrix_data(times_to_evaluate);
 plot(index_data_to_show/index_data_to_show(1),'--', 'LineWidth', 2)
 legend('k = 0.5', 'k=1', 'k=2', 'k=3', 'Index')
-
+%%
 figure(3), clf
-plot(store_weights)
+plot(squeeze(store_weights(:,:,1)))
