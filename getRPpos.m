75949b1ca245cb693c36009bf28680fc615640b1
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-07 12:52:12 +0200

    Solves for RP-weights using KKT-system, commented option for optimization. Not clear yet what w0 should be.

--- getRPpos.m
+++ getRPpos.m
@@ -1,31 +1,38 @@
-function [ RPpos ] = getRPpos(signals, corrMat, params)
+function [ RPpos ] = getRPpos(signals, corrMat, target_volatility)
 
   [T,N] = size(signals);
-  
+ 
   RPpos = nan(T,N);
   
   for t = 2:T
-    if mod(t,100)==0, disp(t); end
-    Q = (corrMat(:,:,t) + corrMat(:,:,t)')/2; %sym
+    Q = corrMat(:,:,t);
+    if mod(t,1000)==0, disp(t); end
     activeI = logical(any(Q).*(~isnan(signals(t,:))));
     if ~any(activeI), continue; end
-    w0 = signals(t,activeI)';
-    warning('off', 'optim:fminunc:SwitchingMethod');
+    w0 = sign(signals(t,activeI)');
     fmin_options = optimset('Display', 'off') ;
-    [w_t, ~, exitflag] = fminunc(@(w)-objective(w),w0,fmin_options);
+%     [w_t, ~, exitflag] = fmincon(@(w)-objective(w),w0,[],[],[],[],[],[],...
+%       @(w)constraint(w,Q(activeI,activeI)),fmin_options);
+    [w_t,~,exitflag] = fsolve(@(w)F(w,Q(activeI,activeI)),w0,fmin_options);
     if exitflag < 1
       fprintf('RP: fmincon failed to find optimum, extitflag: %d \n', exitflag)
     end
     RPpos(t,activeI) = w_t';
-%     portfolio_volatility = sqrt( w_t' * sigma * w_t );
-%     scaled_weights = params.target_volatility / portfolio_volatility * w_t;
-%     w_t = scaled_weights;
-
   end
   
   
   function [value] = objective(w)
     value = sum(log(abs(w)));
+  end
+
+  function [c, ceq] = constraint(w, sigma)
+    c = sqrt(w'*sigma*w)-target_volatility;
+    ceq = [];
+  end
+
+  function [value] = F(w, sigma)
+    n = size(sigma,1);
+    value = w(:).*(sigma*w(:)) - target_volatility^2/n*ones(n,1);
   end
   
 end
