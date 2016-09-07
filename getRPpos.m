b12883776b10c79b08d4d9ef19cd57facb34232e
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-08 00:14:35 +0200

    Modifications to use newton solver

--- getRPpos.m
+++ getRPpos.m
@@ -5,24 +5,16 @@ function [ RPpos ] = getRPpos(signals, corrMat, target_volatility)
   RPpos = nan(T,N);
   
   for t = 2:T
-    Q = (corrMat(:,:,t)+ corrMat(:,:,t)')/2;
     if mod(t,1000)==0, disp(t); end
+    Q = corrMat(:,:,t);
     activeI = logical(any(Q).*(~isnan(signals(t,:))));
     if ~any(activeI), continue; end
-    w0 = sign(signals(t,activeI)');
-    Q(activeI,activeI) = test(Q(activeI,activeI),w0);
-    n = length(w0);
-    fmin_options = optimset('Display', 'off');
-%     options = optimoptions(@fmincon,'MaxIter',2000,'Display','off');
-%     [w_t, ~, exitflag] = fmincon(@(w)-objective(w),w0,[],[],[],[],lb,ub,...
-%       @(w)constraint(w,Q(activeI,activeI)),options);
-    [w_t,~,exitflag] = fsolve(@(w)F(w,Q(activeI,activeI),n),abs(w0),fmin_options);
-    if exitflag < 1
-      fprintf('RP: fmincon failed to find optimum, extitflag: %d \n', exitflag)
-    end
-%     [w_tt, w_t]
-    RPpos(t,activeI) = abs(w_t).*w0;
-%     if sum((w_t.*w0)<0)>0, disp('changed sign'); disp(t); end
+    
+    signal_signs = sign(signals(t,activeI)');
+    adjusted_corrMat = adjustForSigns(Q(activeI,activeI),signal_signs);
+    w0 = ones(length(signal_signs),1);
+    w_t = RP_newton(w0,adjusted_corrMat,target_volatility);
+    RPpos(t,activeI) = w_t(:).*signal_signs;
   end
   
   
@@ -39,12 +31,10 @@ function [ RPpos ] = getRPpos(signals, corrMat, target_volatility)
     value = w(:).*(sigma*w(:)) - target_volatility^2/n*ones(n,1);
   end
 
-  function [mat] = test(mat, signal)
-    for i = 1:size(mat,1)
-      for j = 1:size(mat,2)
-        mat(i,j) = mat(i,j)*sign(signal(i)*signal(j));
-      end
-    end
+  function [C_adj] = adjustForSigns(C, signal)
+    tmp = repmat(signal,1,size(C,1));
+    signs = tmp.*tmp';
+    C_adj = C.*signs;
   end
 
   
