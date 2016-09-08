41ff7b989444cbc672f4d1551b83ee117ce5b6fc
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-08 17:29:43 +0200

    Adds method to calculate holding time of strategy

---
+++ holdingTime.m
@@ -0,0 +1,9 @@
+function [htime_sign, htime] = holdingTime( pos )
+
+[T,N] = size(pos);
+side = sign(pos);
+sign_flips = NansumNan(NansumNan(abs(side(2:T,:)-side(1:T-1,:))));
+htime_sign = 2*T*N/sign_flips;
+htime = NansumNan(NansumNan(abs(pos)))/NansumNan(NansumNan(abs(pos(2:T,:)-pos(1:T-1,:))));
+end
+
