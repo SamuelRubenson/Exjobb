03fe60f0672326e7aa52772ad1b188be883f149e
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-08 11:13:00 +0200

    Nicer prints

--- getRPpos.m
+++ getRPpos.m
@@ -5,7 +5,7 @@ function [ RPpos ] = getRPpos(signals, corrMat, target_volatility)
   RPpos = nan(T,N);
   
   for t = 2:T
-    if mod(t,1000)==0, disp(t); end
+    if mod(t,1000)==0, fprintf('Processing RP-model...(%d/%d)\n',t,T); end
     Q = corrMat(:,:,t);
     activeI = logical(any(Q).*(~isnan(signals(t,:))));
     if ~any(activeI), continue; end
