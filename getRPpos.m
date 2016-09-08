d24a11ed2b0bf4a35ae6ad6412c60f4ea1a2e966
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-08 18:16:54 +0200

    WIP fmincon with gradient

--- getRPpos.m
+++ getRPpos.m
@@ -12,14 +12,30 @@ function [ RPpos ] = getRPpos(signals, corrMat, target_volatility)
     
     signal_signs = sign(signals(t,activeI)');
     adjusted_corrMat = adjustForSigns(Q(activeI,activeI),signal_signs);
-    w0 = ones(length(signal_signs),1);
-    w_t = RP_newton(w0,adjusted_corrMat,target_volatility);
+    %w0 = 0.99*target_volatility/sqrt(sum(sum(adjusted_corrMat)))*ones(length(signal_signs),1);
+    n = length(signal_signs);
+    w0 = abs(signal_signs);% 0.99*target_volatility/sqrt(sum(sum(adjusted_corrMat)))*signal_signs; 
+    
+    options = optimoptions('fmincon', 'GradObj','on', 'Display','off');
+    [w_t,~,exitflag] = fmincon(@(w)objective(w,adjusted_corrMat,n),w0,[],[],[],[],zeros(n,1),[],[],options);
+
+%     warning('off', 'optim:fminunc:SwitchingMethod'); 
+%     [w_t,~,exitflag] = fminunc(@(w)-sum(log(w)) + n/target_volatility^2*(w'*adjusted_corrMat*w - target_volatility^2),...
+%       w0,optimset('Display','off'));
+    % [w_t,~,exitflag] = fsolve(@(w)F(w,Q(activeI,activeI),n),signal_signs,optimset('Display','off')); 
+    
+    %w_t = RP_newton(w0,adjusted_corrMat,target_volatility);
+    
+    
+    %if any(sign(w_t)~=sign(w0)), disp('signChange'); end
+    if exitflag<1, disp('fail'); disp(exitflag); end
     RPpos(t,activeI) = w_t(:).*signal_signs;
   end
   
   
-  function [value] = objective(w)
-    value = sum(log(abs(w)));
+  function [f,g] = objective(w,Q,n)
+    f = -sum(log(w)) + n/target_volatility^2*(w'*Q*w);
+    g = -1./w + 2*n/target_volatility^2*(Q*w);
   end
 
   function [c, ceq] = constraint(w, sigma)
@@ -37,5 +53,4 @@ function [ RPpos ] = getRPpos(signals, corrMat, target_volatility)
     C_adj = C.*signs;
   end
 
-  
 end
