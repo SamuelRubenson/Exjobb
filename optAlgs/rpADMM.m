f640e660196971348096c54ca71782a9f61811f6
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-13 16:00:20 +0200

    Lower tol to ensure convergence. Also fix bug, should maby delete max and the minus term

--- optAlgs/rpADMM.m
+++ optAlgs/rpADMM.m
@@ -1,13 +1,13 @@
 function [w] = rpADMM(w0,Q,mu, signal)
 z=w0(:); u=zeros(length(w0),1);  
-tol = 10^-6; max_norm = 1;
+tol = 10^-10; max_norm = 1;
 rho = 0.5;
 
 [H,D] = eig(Q);
 inverse = H*diag(1./(diag(2*mu*D)+rho))*H'; 
 while max_norm>tol
   x_new = max( [1/2*((z-u) + sqrt((z-u).^2 + 4*abs(signal(:))/rho)),...
-    1/2*((z-u) - sqrt((z-u).^2 + 4/rho))], [], 2);
+    1/2*((z-u) - sqrt((z-u).^2 + 4*abs(signal(:))/rho))], [], 2);
   z_new = rho*inverse*(x_new+u);
   u_new = u + (x_new-z_new);
   s = norm(rho*(z-z_new));
