59016102f8f92ce6d3c5d1970d848730f1864281
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-13 20:29:27 +0200

    ADMM: Update penalty

--- optAlgs/rpADMM.m
+++ optAlgs/rpADMM.m
@@ -5,21 +5,19 @@ rho = 0.5;
 tau = 2; mult = 10;
 
 [H,D] = eig(Q);
-inverse = H*diag(1./(diag(2*mu*D)+rho))*H'; 
+%inverse = H*diag(1./(diag(2*mu*D)+rho))*H'; 
 while max_norm>tol
   x_new = max( [1/2*((z-u) + sqrt((z-u).^2 + 4*abs(signal(:))/rho)),...
     1/2*((z-u) - sqrt((z-u).^2 + 4*abs(signal(:))/rho))], [], 2);
-  z_new = rho*inverse*(x_new+u);
+  z_new = rho*H*diag(1./(diag(2*mu*D)+rho))*H'*(x_new+u);
   u_new = u + (x_new-z_new);
   s = norm(rho*(z-z_new));
   r =  norm(u_new-u);
-%   if r/s > mult
-%     disp('a')
-%     rho = tau*rho; u_new = u_new/tau;
-%   elseif r/s < 1/mult
-%     disp('b')
-%     rho = rho/tau; u_new = u_new*tau;
-%   end
+  if r/s > mult
+    rho = tau*rho; u_new = u_new/tau;
+  elseif r/s < 1/mult
+    rho = rho/tau; u_new = u_new*tau;
+  end
   max_norm = max(s,r);
   z = z_new; u = u_new;
 end
