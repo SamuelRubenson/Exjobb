ecac743b6fcf530c5d0a22a49d8c084e420dcb23
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-18 22:07:39 +0200

    Adds colors and fill weight distribution plot

--- visualizePerformance.m
+++ visualizePerformance.m
@@ -1,4 +1,13 @@
 function [] = visualizePerformance( outCome, dates, assetClasses )
+fill_between_lines = @(X,Y1,Y2,C) fill( [X fliplr(X)],  [Y1 fliplr(Y2)], C );
+colors =[      0,    0.4470,    0.7410;
+         0.8500,    0.3250,    0.0980;
+         0.9290,    0.6940,    0.1250;
+         0.4940,    0.1840,    0.5560;
+         0.4660,    0.6740,    0.1880;
+         0.3010,    0.7450,    0.9330;
+         0.6350,    0.0780,    0.1840];
+    
 for i = 1:4, figure(i), clf; end
 models = fieldnames(outCome.Models);
 sharpe_ratios = zeros(length(models), 1);
@@ -23,16 +32,21 @@ set(gca,'xtick', 1:length(models),'xticklabel', models)
 ylabel('Sharpe Ratio')
 
 
+
+figure(5), clf
 for iModel=1:length(models)
   model_pos = outCome.Models.(models{iModel}).pos;
   [data,groups] = grpstats(abs(model_pos'),assetClasses',{'sum', 'gname'});
-  figure(), title(models{iModel}),hold on
-  plot(cumsum(abs(data')./repmat(sum(abs(data'),2),1,length(groups)),2))
+  data(isnan(data)) = 0;
+  norm_data = sum(abs(data'),2); norm_data(norm_data==0) = 1;
+  plot_data = [zeros(size(data',1),1), cumsum(abs(data')./repmat(norm_data,1,length(groups)),2)];
+  subplot(2,2,iModel), title(models{iModel}),hold on
+  for iClass = 2:size(plot_data,2)
+    jbfill(1:size(plot_data,1), plot_data(:,iClass)', plot_data(:, iClass-1)', colors(iClass-1,:));
+  end
   ylim([0,1])
-  legend(groups)
-  %boxplot(model_pos./repmat(sum(abs(model_pos),2),1,size(model_pos,2)),assetClasses)
 end
-
+legend(groups)
 
     
 
