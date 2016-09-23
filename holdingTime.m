6acb13d5fa6674b8017e79423ba41bd88c605c25
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-23 11:56:53 +0200

    Holdingtime bugfix, adds factor 2

--- holdingTime.m
+++ holdingTime.m
@@ -4,6 +4,6 @@ function [htime, htime_sign] = holdingTime( pos )
 side = sign(pos);
 sign_flips = NansumNan(NansumNan(abs(side(2:T,:)-side(1:T-1,:))));
 htime_sign = 2*T*N/sign_flips;
-htime = NansumNan(NansumNan(abs(pos)))/NansumNan(NansumNan(abs(pos(2:T,:)-pos(1:T-1,:))));
+htime = 2*NansumNan(NansumNan(abs(pos)))/NansumNan(NansumNan(abs(pos(2:T,:)-pos(1:T-1,:))));
 end
 
