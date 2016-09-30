7831278f5251c66a111ec22c188d6637a9416de8
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-09-30 15:19:48 +0200

    WIP LES with linprog dual simplex

--- getLESpos.m
+++ getLESpos.m
@@ -2,28 +2,43 @@ function [pos] = getLESpos( dZ, signals, corrMat, assetClasses,  target_volatili
 
 [T,N] = size(signals);
 
+options = optimoptions('linprog','Algorithm','dual-simplex', 'Display', 'off');
+%options = optimoptions('linprog','Display', 'off');
 
 pos = nan(T,N);
 q = 252;
 beta = 0.6;
-
-for t = q+1:T
+parfor t = q+1:T
   y = dZ(t-q+1:t,:);
-  activeI = all(~isnan(y),1);
+  
+  s = signals(t,:);
+  activeI = logical(all(~isnan(y),1).*(~isnan(s)));
+  if ~any(activeI), continue; end
+  
   n = sum(activeI);
-  %A = [-y(:,activeI), -ones(q,1), -eye(q); -signals(t,activeI), 0, zeros(1,q)];
-  %b = [zeros(q, 1); -1];
-  %lb = [-inf*ones(n,1); -inf; zeros(q,1)];
-  %f = [zeros(n,1); 1; 1/(q*(1-beta))*ones(q,1)];
-  %opt = linprog(f,A,b,[],[],lb,[]);
-  %opt = lesADMM(f, y, q);
-  x = opt(1:n);
-  x = x*target_volatility/sqrt(x(:)'*corrMat(activeI,activeI,t)*x(:));
-  alpha = opt(n+1);
-  pos(t,activeI) = x;
-  t
+  %A = [-y(:,activeI), -ones(q,1), -eye(q); diag(-s(activeI)'), zeros(n,1), zeros(n,q)];
+  %b = [zeros(q, 1); -0.01*ones(n,1)];
+  A = [-y(:,activeI), -ones(q,1), -eye(q); -s(activeI), 0, zeros(1,q)];
+  b = [zeros(q, 1); -1];
+  lb = [-inf*ones(n,1); -inf; zeros(q,1)];
+  f = [zeros(n,1); 1; 1/(q*(1-beta))*ones(q,1)];
+  [opt, fval, exitflag] = linprog(f,A,b,[],[],lb,[], zeros(n,1), options);
+  %opt = lesADMM(y, q, activeI);
+  %opt = fmincon(@(x) x(1) + 1/(q*(1-beta))*sum(max([y(:,activeI)*x(2:end), zeros(q,1)],[],2)),...
+  %  1/n*ones(n+1,1));
+  
+  if exitflag>=0
+    x = opt(1:n);
+    alpha = opt(n+1);
+    x = x*target_volatility/sqrt(x(:)'*corrMat(activeI,activeI,t)*x(:));
+    out = nan(1,N);
+    out(activeI) = x;
+    pos(t,:) = out;
+    if exitflag==0, disp('Max-iter hit'); end
+  else
+    fprintf('No solution, exitflag: %d \n', exitflag)
+  end
 end
-
 
 
 end
--- run.m
+++ run.m
@@ -25,11 +25,11 @@ RP = {'RP', RP_Params};
 RPmod_Params = struct('lambda', 0.5, 'regCoeffs', 10^10);
 RPM = {'RPmod', RPmod_Params};
 
-LES_Params = struct('lambdaMV', 0.5, 'lambdaRP', 0);
+LES_Params = struct('lambdaMV', 0.3);
 LES = {'LES', LES_Params};
 
-start = 9000; X = [1:3, 6:9, 11, 14 ,22:23, 26, 4, 10, 16:18 27, 29, 34, 37, 40, 41:42, 5, 13, 15, 21, 25, 31, 63:68, 12, 19, 20, 24, 28, 32, 33, 35, 36, 38, 43:44];
-outCome = evaluatePerformance(Open(start:end,X), High(start:end,X), Low(start:end,X), Close(start:end,X), Config, assetClasses(X), TF{:}, MV{:}, RP{:}, LES{:});
+start = 1; X = 1:74;% [1:3, 6:9, 11, 14 ,22:23, 26, 4, 10, 16:18 27, 29, 34, 37, 40, 41:42, 5, 13, 15, 21, 25, 31, 63:68, 12, 19, 20, 24, 28, 32, 33, 35, 36, 38, 43:44];
+outCome = evaluatePerformance(Open(start:end,X), High(start:end,X), Low(start:end,X), Close(start:end,X), Config, assetClasses(X), TF{:}, MV{:}, LES{:});
 
 visualizePerformance(outCome, dates(start:end), assetClasses(X));
 
