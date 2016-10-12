0784a8c27753d705197f22fa11e159c7051a2fe1
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-10-12 09:01:48 +0200

    WIP tail min

--- tests/probMat.m
+++ tests/probMat.m
@@ -1,42 +1,44 @@
-function [testPos, times, assets, lookBack] = probMat(outCome, Config)
+function [testPos, times, assets, lookBack] = probMat(outCome, Config , Y, M)
 clc
-assets = 1:74;
-times = 1:3000;
-quantiles = 0.05;
+options = optimoptions('fmincon', 'Display', 'off');
+assets = find(M);
+times = 1:5000;
+%quantiles = 0.05;
 
-dZ = outCome.General.dZ(times,assets);  nPoints = 5000; lookBack = 504;
+dZ = outCome.General.dZ(times,M);  nPoints = 5000; lookBack = 504;
 signals = outCome.Models.TF.pos(times,assets);
-Q = outCome.General.corr;
+Q = outCome.General.corr(M,M,:);
 
 testPos = nan(length(times), length(assets));
-parfor t = times(lookBack:end)
+for t = times(lookBack:end)
 t
 X = dZ(t-lookBack+1:t,:);
 s = signals(t,:);
 activeI = logical(all(~isnan(X),1).*(~isnan(s)));
 if ~any(activeI), continue; end
-X = X(:,activeI);
-[T,N] = size(X);
-% U = nan(T,N);
-F = zeros(T,N);
-for iN = 1:N
-  F(:,iN) = ksdensity(X(:,iN),X(:,iN),'function','cdf');
-end
-[Rho,nu] = copulafit('t',F,'Method','ApproximateML');
-Y_u = copularnd('t', Rho, nu, nPoints);
-
-Y = zeros(size(Y_u));
-for iN = 1:N
-  Y(:,iN) = ksdensity(X(:,iN),Y_u(:,iN),'function','icdf');
-end
-Y = Y.*(abs(Y_u-0.5)>=0.5-quantiles); %quantiles
+% X = X(:,activeI);
+% [T,N] = size(X);
+% % U = nan(T,N);
+% F = zeros(T,N);
+% for iN = 1:N
+%   F(:,iN) = ksdensity(X(:,iN),X(:,iN),'function','cdf');
+% end
+% [Rho,nu] = copulafit('t',F,'Method','ApproximateML');
+% Y_u = copularnd('t', Rho, nu, nPoints);
+% 
+% Y = zeros(size(Y_u));
+% for iN = 1:N
+%   Y(:,iN) = ksdensity(X(:,iN),Y_u(:,iN),'function','icdf');
+% end
+% Y = Y.*(abs(Y_u-0.5)>=0.5-quantiles); %quantiles
 
 %Y = sign(Y_u-0.5).*((Y_u-0.5).^2);
 %Y = (Y_u-0.5).*(abs(Y_u-0.5)>=0.5-quantiles); %use
 %Y = Y.*repmat(sign(s(activeI)), nPoints, 1);
 
-c = ones(nPoints,1);
-x = fmincon(@(x) norm(Y*x,2) , s(activeI)'/norm(s(activeI)), -s(activeI),-1, [], [], [], []);
+%c = ones(nPoints,1);
+[x, ~, exitflag] = fmincon(@(x) norm(Y(:,activeI)*x,2) , s(activeI)'/norm(s(activeI)), -s(activeI),-1, [], [], [], [], [], options);
+if exitflag<=0, disp('-------------------------------'); disp(exitflag); end
 %x = fmincon(@(x) norm(Y*x,2) , 0.1*ones(N,1), [],[], ones(1,N), 1, zeros(N,1), []);
 out = nan(length(assets),1);
 out(activeI) = x(:)*Config.target_volatility/sqrt(x(:)'*Q(activeI,activeI,t)*x(:)); %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- tests/testTailMin.m
+++ tests/testTailMin.m
@@ -1,4 +1,30 @@
-[testPos, times, assets, lookBack] = probMat(outCome, Config);
+
+M = ~isnan(outCome.General.dZ(7000,:));
+X = outCome.General.dZ(7000:end,M);
+
+[T,N] = size(X);
+% U = nan(T,N);
+F = zeros(T,N);
+for iN = 1:N
+  F(:,iN) = ksdensity(X(:,iN),X(:,iN),'function','cdf');
+end
+[Rho,nu] = copulafit('t',F,'Method','ApproximateML');
+Y_u = copularnd('t', Rho, nu, 10000);
+
+Y = zeros(size(Y_u));
+for iN = 1:N
+  iN
+  Y(:,iN) = ksdensity(X(:,iN),Y_u(:,iN),'function','icdf');
+end
+Y = Y.*(abs(Y_u-0.5)>=0.5-0.05); %quantiles
+
+
+
+%%
+
+[testPos, times, assets, lookBack] = probMat(outCome, Config ,Y, M);
+
+
 %%
 [sharpe, equityCurve, htime, rev] = indivitualResults(testPos, Config.cost, Open(times,assets), Close(times,assets), outCome.General.std(times,assets), Config.riskAdjust);
 TFpos = outCome.Models.TF.pos(times,assets); TFpos(1:lookBack,:) = NaN;
@@ -10,5 +36,8 @@ plot(dates(times), equityCurveTF)
 plot(dates(times),equityCurve)
 
 sRev = sort(diff(equityCurve(~isnan(equityCurve)))); sRevTF = sort(diff(equityCurveTF(~isnan(equityCurveTF))));
-take = 4;
+take = 25;
 meanQuatile = [mean(sRev(1:take)), mean(sRevTF(1:take))]
+
+
+
