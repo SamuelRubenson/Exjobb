0b80ec54ceb8a435398f481ebf53a33f5dd14c9f
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-10-13 16:06:32 +0200

    Working lesADMM for tweek

--- optAlgs/lesADMM.m
+++ optAlgs/lesADMM.m
@@ -1,20 +1,23 @@
-function [w] = lesADMM(y, q, activeI)
-  beta = 0.6;
-  A = [-ones(q,1), -y(:,activeI), -eye(q)];
-  c = [1; zeros(n,1); 1/(q*(1-beta))*ones(q,1)];
-  w0 = ones(size(c));
+function [w] = lesADMM(Y, mu)
+  
+  A = Y'*Y;
+  C = 1;
+  mu = mu(:);
+  lambda = C/(mu'*(A\mu));
+  w0 = mu/norm(mu);
   
   z=w0(:); u=zeros(length(w0),1);  
   tol = 10^-4; max_norm = 1;
   rho = 0.5;
   tau = 2; mult = 10;
   
+  [H,D] = eig(A);
   while max_norm>tol
-    x_new = A\(-c/rho + z - u);
-    x_new(end-q+1:end) = max([x_new(end-q+1:end), zeros(q,1)],[],2);
-    Ax = A*x_new;
-    z_new = max( [Ax+u zeros(size(A,1),1)],[],2);
-    u_new = u + (Ax-z_new);
+    x_new = H*diag(1./(diag(D) + rho))*H'*(z - u);
+    %x_new(end-q+1:end) = max([x_new(end-q+1:end), zeros(q,1)],[],2);
+    %Ax = A*x_new;
+    z_new = lambda/rho*mu + rho*(x_new + u);
+    u_new = u + (x_new - z_new);
     s = norm(rho*(z-z_new));
     r =  norm(u_new-u);
     [rho, u_new] = updateRho(s,r,rho,u_new);
