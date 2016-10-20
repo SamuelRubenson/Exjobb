a5f37ea60661a2b458949703d0fbd08c529c7410
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-10-20 16:37:58 +0200

    Helpers, change output structure in rollingSharpe parts, +imgsc plot

---
+++ helpers/imageSCplot.m
@@ -0,0 +1,28 @@
+[T,N] = size(outCome.General.dZ);
+classes = unique(assetClasses);
+nC = numel(classes);
+cInd = nan(nC,N); 
+for i = 1:nC
+  cInd(i,:) = cellfun(@(c)strcmp(c,classes{i}), assetClasses);
+end
+%%
+close all
+models = fieldnames(outCome.Models);
+
+
+for iModel = 1:numel(models)-1
+pos_ordered = [];
+names_ordered = {};
+for iC = 1:nC
+  ind = logical(cInd(iC,:));
+  classPos = outCome.Models.(models{iModel}).pos(:,ind);
+  %[~, order] = sort(nanmean(abs(classPos),1)); 
+  %classPos = classPos./repmat(max(abs(classPos),[],1),T,1); % scale
+  pos_ordered = [pos_ordered, classPos];
+  names_ordered = [names_ordered, names(ind)];
+end
+figure(), hold on, title(models{iModel})
+imagesc(pos_ordered')
+colorbar
+set(gca,'ytick',1:N,'yticklabel',regexprep(names_ordered,'\_','\\_'),'ylim',[0 length(names)+2],'ygrid','on');
+end
--- helpers/rollSharpe.m
+++ helpers/rollSharpe.m
@@ -1,16 +1,18 @@
-function [ sh, parts ] = rollSharpe(rev, nYears)
+function [ sh, storeMean, storeStd ] = rollSharpe(rev, nYears)
 
 T = numel(rev);
 tau = 252*nYears;
 sh = nan(T,1);
-parts = nan(T,2);
+storeMean = nan(T,1);
+storeStd = nan(T,1);
 start = find(~isnan(rev),1,'first');
 
 for t = tau+start:T
   mu = nanmean(rev(t-tau:t-1));
   std = nanstd(rev(t-tau:t-1));
   sh(t) = mu/std;
-  parts(t,:) = [mu, std];
+  storeMean(t) = mu;
+  storeStd(t) = std;
 end
 sh = sh*sqrt(252);
 
