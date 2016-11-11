d3c787d613e98465375c739566b00704f0167cb4
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-11-11 13:59:04 +0100

    changeCost; adds revenue

--- changeCost.m
+++ changeCost.m
@@ -2,12 +2,13 @@ function [ outCome ] = changeCost(outCome,cost, riskAdjust, Open,Close)
 
 models = fieldnames(outCome.Models);
 
-for iModel = 1:length(models);
-[sharpe, equityCurve, htime] = indivitualResults(outCome.Models.(models{iModel}).pos,...
+for iModel = 1:length(models)
+[sharpe, equityCurve, htime, r] = indivitualResults(outCome.Models.(models{iModel}).pos,...
   cost, Open, Close, outCome.General.std, riskAdjust);
 outCome.Models.(models{iModel}).sharpe = sharpe;
 outCome.Models.(models{iModel}).equityCurve = equityCurve;
 outCome.Models.(models{iModel}).htime = htime;
+outCome.Models.(models{iModel}).rev = r;
 end
 
 
