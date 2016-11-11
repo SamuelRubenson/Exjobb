099787a6b0b8821d2a598e345a31d7f09fd6e50c
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-11-11 14:02:01 +0100

    helpers for cluster run

--- helpers/imageSCplot.m
+++ helpers/imageSCplot.m
@@ -6,18 +6,18 @@ for i = 1:nC
   cInd(i,:) = cellfun(@(c)strcmp(c,classes{i}), assetClasses);
 end
 %%
-close all
+%close all
 models = fieldnames(outCome.Models);
 
 
-for iModel = 1:numel(models)-1
+for iModel = 1:numel(models)
 pos_ordered = [];
 names_ordered = {};
 for iC = 1:nC
   ind = logical(cInd(iC,:));
   classPos = outCome.Models.(models{iModel}).pos(:,ind);
   %[~, order] = sort(nanmean(abs(classPos),1)); 
-  %classPos = classPos./repmat(max(abs(classPos),[],1),T,1); % scale
+  classPos = classPos./repmat(max(abs(classPos),[],1),T,1); % scale
   pos_ordered = [pos_ordered, classPos];
   names_ordered = [names_ordered, names(ind)];
 end
