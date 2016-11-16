967ad1adb230ec67f137a434e75e539e089fc516
Samuel Rubenson <rubenson.samuel@gmail.com>
2016-11-16 16:17:53 +0100

    Greedy iteration of signs

--- getRPMODpos.m
+++ getRPMODpos.m
@@ -4,34 +4,27 @@ function [ RPpos ] = getRPMODpos(signals, corrMat, target_volatility, lambda, re
   [T,N] = size(signals);
   RPpos = nan(T,N);
   
-  for t = 2:T
-    if mod(t,1000)==0, fprintf('Processing RPmod-model...(%d/%d)\n',t,T); end
-    Q = addToDiag(corrMat(:,:,t), lambda);
+  parfor t = 2:T
+    t
+    %if mod(t,1000)==0, fprintf('Processing RPmod-model...(%d/%d)\n',t,T); end
+    Q = corrMat(:,:,t) + lambda*eye(N);
     activeI = logical(any(Q).*(~isnan(signals(t,:))));
     if ~any(activeI), continue; end
     signal = signals(t,activeI)';
 
 
-    w_init = RP_ADMM(Q(activeI, activeI), signal, target_volatility, 'RPmod');
+    w_init = RP_ADMM(Q(activeI, activeI), target_volatility, signal, 'RPmod');
     expRet_init = signal'*w_init;
     w = iterateSigns(Q(activeI,activeI), signal, w_init, expRet_init, signal);
-
-    RPpos(t,activeI) = w*target_volatility/sqrt(w(:)'*corrMat(activeI,activeI,t)*w(:));
+    %w = w_init;
+    
+    thisPos = nan(N,1);
+    thisPos(activeI) = w*target_volatility/sqrt(w(:)'*corrMat(activeI,activeI,t)*w(:));
+    RPpos(t,:) = thisPos;
   end
   
   
-  function [w_best] = iterateSigns(Q, signal, w_best, expRet_best, signal_current)
-    for iS = 1:length(signal)
-      mod_signal = signal_current;
-      mod_signal(iS) = -signal_current(iS);
-      w_new = RP_ADMM(Q, mod_signal, target_volatility, 'RPmod');
-      expRet = signal'*w_new;
-      if expRet > expRet_best
-        w_best = iterateSigns(Q, signal, w_new, expRet, mod_signal);
-        break
-      end
-    end
-  end
+  
 
   
   function [C_adj] = adjustForSigns(C, signal)
@@ -39,15 +32,15 @@ function [ RPpos ] = getRPMODpos(signals, corrMat, target_volatility, lambda, re
     signs = tmp.*tmp';
     C_adj = C.*signs;
   end
-
-  function [] = checkSolution(w_t, adjusted_corrMat, signal)
-    if abs(w_t'*adjusted_corrMat*w_t-target_volatility^2)>0.01
-      disp('sigma error'); disp([w_t'*adjusted_corrMat*w_t, target_volatility^2]); 
-    end
-    if any(abs(diff(w_t.*(adjusted_corrMat*w_t)./(abs(signal))))>0.01)
-      disp('not mcr'); disp(w_t.*(adjusted_corrMat*w_t)./(abs(signal))); 
-    end
-  end
+% 
+%   function [] = checkSolution(w_t, adjusted_corrMat, signal)
+%     if abs(w_t'*adjusted_corrMat*w_t-target_volatility^2)>0.01
+%       disp('sigma error'); disp([w_t'*adjusted_corrMat*w_t, target_volatility^2]); 
+%     end
+%     if any(abs(diff(w_t.*(adjusted_corrMat*w_t)./(abs(signal))))>0.01)
+%       disp('not mcr'); disp(w_t.*(adjusted_corrMat*w_t)./(abs(signal))); 
+%     end
+%   end
 
   function[regQ] = addToDiag(Q, lambda)
     regQ = Q + lambda*diag(diag(Q));
